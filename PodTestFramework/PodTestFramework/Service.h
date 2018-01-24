a6e14c0de7db0ec646b513d00585a42da559eb46
Nik Cane <nikcane@Niks-MacBook-Pro-2.local>
2018-01-24 11:41:56 +0200

    add framework

---
+++ PodTestFramework/PodTestFramework/Service.h
@@ -0,0 +1,15 @@
+//
+//  Service.h
+//  PodTestFramework
+//
+//  Created by Nik Cane on 1/24/18.
+//  Copyright © 2018 boarddev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Service : NSObject
+
+- (NSString *) doSomething;
+
+@end
---
+++ PodTestFramework/PodTestFramework/Service.m
@@ -0,0 +1,17 @@
+//
+//  Service.m
+//  PodTestFramework
+//
+//  Created by Nik Cane on 1/24/18.
+//  Copyright © 2018 boarddev. All rights reserved.
+//
+
+#import "Service.h"
+
+@implementation Service
+
+- (NSString *) doSomething {
+  return @"Did something";
+}
+
+@end
